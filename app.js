6c185c2a57b84e4b66c955a91198fbdf4a5f3679
Zihad550 <dev.jehadhossain@gmail.com>
2023-03-02 21:40:48 +0600

    feat: copy color to clipboard by clicking a btn

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ window.onload = () => {
 
 function main() {
   const root = document.getElementById('root');
-  const btn = document.getElementById('change-btn');
+  const changeBtn = document.getElementById('change-btn');
   const output = document.getElementById('output');
+  const copyBtn = document.getElementById('copy-btn');
 
-  btn.addEventListener('click', function () {
+  changeBtn.addEventListener('click', function () {
     const bgColor = generateHexColor();
     root.style.backgroundColor = bgColor;
     output.value = bgColor;
+  });
+
+  copyBtn.addEventListener('click', function () {
+    navigator.clipboard.writeText(output.value);
   });
 }
 
